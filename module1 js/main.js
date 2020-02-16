96f2cc9fb753c67c1039891fb73724f744059b1d
Agent222222 <56441631+Agent222222@users.noreply.github.com>
2020-02-16 17:18:51 +0200

    Add files via upload

---
+++ module1 js/main.js
@@ -0,0 +1,91 @@
+// // let str;
+// // str="priwet";
+// // let txt;
+// // txt=123;
+// // let flag;
+// // flag=true;
+// // let num;
+// // num="true"
+// // console.log(typeof(txt));
+// // console.log(typeof(flag));
+// // console.log(typeof(num));
+// // console.log(typeof(str));
+
+// let flag=10;
+// let txt="tort";
+// let f=true;
+// let g=6;
+// let wr="work";
+// const n=8;
+// const u=false;
+// const m="logo";
+// const nu=2;
+// const um=true;
+
+
+// let ol=15;
+// let kol=14;
+// let sun=kol+ol;
+// console.log(sun);
+// let c=108;
+// let b=223;
+// let a=2;
+// let d=5;
+// let g=c+b-a*d;
+// alert(g);
+
+
+// let a1=5+3;
+//     a2=5-3;
+//     a3=5*3;
+//     a4=5/3;
+//     a5=5^3;
+// console.log(a1);
+// console.log(a2);
+// console.log(a3);
+// console.log(a4);
+// console.log(a5);
+
+
+// let a6=5%3;
+//     a7=3%5;
+//     a8=5%'3';
+//     a9='5'%3;
+//     a10=75+"kg";
+// alert(a6);
+// alert(a7);
+// alert(a8);
+// alert(a9);
+// alert(a10);
+
+
+// let height=23;
+//     width=10;
+//     s=height*width;
+//     alert(s);
+
+
+// let heightC=10;
+//     dC=4;
+//     const p=3.14;
+//     v=dC*heightC*p;
+//     alert(v);
+
+
+// let n=3;
+//     m=4;
+//     k=Math.sqrt((Math.pow(3,2))+(Math.pow(4,2)))
+//     console.log(k);
+
+
+// let s=2000;
+//     p=10;
+//     years=5;
+//     perepl=((s/100*p)*5);
+//     console.log(perepl);
+
+
+let nfiB=12;
+   el=12;
+   fn
+
