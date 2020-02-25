8f8684976b597f6cba53be40ea295548608fd554
Agent222222 <56441631+Agent222222@users.noreply.github.com>
2020-02-25 22:04:18 +0200

    Add files via upload

---
+++ module1_js/main.js
@@ -0,0 +1,154 @@
+// // let str="priwet";
+// // let num=123;
+// // let flag=true;
+// // let txt="true"
+// // console.log(typeof(str));
+// // console.log(typeof(txt));
+// // console.log(typeof(flug));
+// // console.log(typeof(num));
+
+// let flag=10;
+// let txt="tort";
+// let boolean=true;
+// let num=6;
+// let word="work";
+// const 1num=8;
+// const 2boolean=false;
+// const string="logo";
+// const 2num=2;
+// const 3boolean=true;
+
+
+// let ol=15;
+// let kol=14;
+// let sun=kol+ol;
+// console.log(sun);
+// let c=108;
+// let b=223;
+// let a=2;
+// let d=5;
+// let g=c+b-a*d;
+// alert(g);
+
+
+// let a1=5+3;
+//     a2=5-3;
+//     a3=5*3;
+//     a4=5/3;
+//     a5=5^3;
+// console.log(a1);
+// console.log(a2);
+// console.log(a3);
+// console.log(a4);
+// console.log(a5);
+
+
+// let a6=5%3;
+//     a7=3%5;
+//     a8=5%'3';
+//     a9='5'%3;
+//     a10=75+"kg";
+// alert(a6);
+// alert(a7);
+// alert(a8);
+// alert(a9);
+// alert(a10);
+
+
+// let height=23;
+//     width=10;
+//     s=height*width;
+//     alert(s);
+
+
+// let heightC=10;
+//     dC=4;
+//     const p=3.14;
+//     v=Math.pow(4,2)*heightC*p;
+//     alert(v);
+
+
+// let n=3;
+//     m=4;
+//     k=Math.sqrt((Math.pow(3,2))+(Math.pow(4,2)))
+//     console.log(k);
+
+
+// let s=2000;
+//     p=10;
+//     years=5;
+//     perepl=((s/100*p)*5);
+//     console.log(perepl);
+
+
+// let nfiB=12;
+//    el=12;
+//    fn
+
+// number1
+
+// let k1=console.log(Number.parseInt('5px'));
+//     k2=console.log(Number.parseInt('12.45asdwe12'));  
+//     k3=console.log(Number.parseInt('12djd334'));   
+//     k4=console.log(Number.parseInt('qwqweewq'));
+   
+// number2
+
+// let k1=console.log(Number.parseFloat('5px'));
+//     k2=console.log(Number.parseFloat('12.45asdwe12'));  
+//     k3=console.log(Number.parseFloat('12djd334'));   
+//     k4=console.log(Number.parseFloat('qwqweewq'));
+
+// number3
+
+// let random = Math.floor(Math.random() * (10 - 1) + 1);
+// console.log(Math.pow(random,2));
+
+// number4
+
+// let word="My ",
+//     string="name ",
+//     secword="is ";
+//     fullName = word + string + secword + 'Viktor';  
+//     console.log(fullName)
+
+//   number5  
+
+// result = 5 + 5 + '5';
+// console.log(result);
+
+// number6
+
+// const num = 20;
+// const result = num >10 && num < 30;
+// console.log(result);
+
+// number7
+
+// const num = 40;
+// const result = num < 10 || num > 30;
+// console.log(result);
+
+// number8
+
+// console.log(!true);
+// console.log(!false); 
+// console.log(!1); 
+// console.log(!0);
+
+// Megatask
+
+// let a = -3,
+//     b = -3,
+//     c = 8;
+//     a=a*(-1);
+//     b=b*(-1);
+//     c=c*(-1);
+//  z = Math.pow(b,2);
+//  g = 4 * a * c;
+//  y = Math.sqrt(z - g);
+//  n = 2 * a;
+//  x1 = ((-b)-y)/n;
+//  x2 = ((-b)+y)/n;         
+// console.log(x1);
+// console.log(x2); 
