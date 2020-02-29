00e4616ffa8ec6b26160e253df1c78ab03951f9e
Agent222222 <56441631+Agent222222@users.noreply.github.com>
2020-02-29 17:02:58 +0200

    Add files via upload

---
+++ module_2js/main.js
@@ -0,0 +1,67 @@
+// number1
+
+// let k1=console.log(Number.parseInt('5px'));
+//     k2=console.log(Number.parseInt('12.45asdwe12'));  
+//     k3=console.log(Number.parseInt('12djd334'));   
+//     k4=console.log(Number.parseInt('qwqweewq'));
+   
+// number2
+
+// let k1=console.log(Number.parseFloat('5px'));
+//     k2=console.log(Number.parseFloat('12.45asdwe12'));  
+//     k3=console.log(Number.parseFloat('12djd334'));   
+//     k4=console.log(Number.parseFloat('qwqweewq'));
+
+// number3
+
+// let random = Math.floor(Math.random() * (10 - 1) + 1);
+// console.log(Math.pow(random,2));
+
+// number4
+
+// let word="My ",
+//     string="name ",
+//     secword="is ";
+//     fullName = word + string + secword + 'Viktor';  
+//     console.log(fullName)
+
+//   number5  
+
+// result = 5 + 5 + '5';
+// console.log(result);
+
+// number6
+
+// const num = 20;
+// const result = num >10 && num < 30;
+// console.log(result);
+
+// number7
+
+// const num = 40;
+// const result = num < 10 || num > 30;
+// console.log(result);
+
+// number8
+
+// console.log(!true);
+// console.log(!false); 
+// console.log(!1); 
+// console.log(!0);
+
+// Megatask
+
+// let a = -3,
+//     b = -3,
+//     c = 8;
+//     a=a*(-1);
+//     b=b*(-1);
+//     c=c*(-1);
+//  z = Math.pow(b,2);
+//  g = 4 * a * c;
+//  y = Math.sqrt(z - g);
+//  n = 2 * a;
+//  x1 = ((-b)-y)/n;
+//  x2 = ((-b)+y)/n;         
+// console.log(x1);
+// console.log(x2); 
