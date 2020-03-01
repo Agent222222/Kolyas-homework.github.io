014e563b7f8344409bdf3d22be8e46629b12fb58
Agent222222 <56441631+Agent222222@users.noreply.github.com>
2020-03-01 18:01:28 +0200

    Add files via upload

---
+++ module_3js/main.js
@@ -0,0 +1,56 @@
+// let data = 1;
+// if (data<2){console.log(data)} 
+//  else{console.log("tochno net")};
+
+// number 2
+
+// let data = 1;
+// if (data%2==0){console.log(data)} 
+//  else{console.log("uppss")};
+
+//  number 3
+
+//  let cash=5;
+//  if (5%2==0){console.log(cash+"grn")}
+//   else{console.log(cash+"euro")}; 
+
+//   number 4
+
+// let number=55;
+// if (number>0 && number<16){
+//     console.log("child");
+// }else if(60>number && number>17){
+//      console.log("adult");
+//     } else if(10000>number && number>60){
+//       console.log("old");
+//     }
+
+// number 5
+
+// let a=Math.floor(Math.random() * 2),
+// b=Math.floor(Math.random() * 2);
+// if(a===b){console.log("fart")}
+// else{console.log("not fart")};
+
+// number 6
+
+// let name=['K','o','l','y','a'],
+//     surname=['M','u','z','y','k','a'];
+//     name=(name.length);
+//     surname=(surname.lendth);
+// if(name>4 && surname>5){console.log(name+surname)}
+// else{console.log("UPS")};
+
+// number 7
+
+// let a=Math.floor((Math.random() * 5)+1);
+// if(a===1){console.log("its one")}
+// else if(a===2){console.log("its two")}
+// else if(a===3){console.log("its three")}
+// else if(a===4){console.log("its four")}
+// else if(a===5){console.log("its five")};
+
+// number 8
+let title = "How old are you?"
+let result = prompt(title)
+
