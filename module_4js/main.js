6184472f849d899f28c49ba05eaa4bb90bd61061
Agent222222 <56441631+Agent222222@users.noreply.github.com>
2020-03-08 17:54:41 +0200

    Add files via upload

---
+++ module_4js/main.js
@@ -0,0 +1,67 @@
+// number 1
+ 
+// for(let i = 0; i<11; i++){
+//     console.log(i);
+// }
+
+// number 2
+
+
+// for(let i =1; i<10; i++ ){
+   
+//     if(i % 2===0){
+//         console.log("number can delet on two");
+//     }
+//     else{console.log("number can't delete on two");}
+// }
+
+// number 3
+
+// let num=20
+// while(num < 28){
+//     num++;
+//     console.log(num);
+// }
+
+// number 4
+
+
+// let name = prompt("Name of your hero");
+// while(name.length<6){
+//     console.log(name);
+//     name = prompt("Name of your hero");  
+// }
+
+// number 5
+
+// let arr =[5,3,6,2,4], result=0;
+// for(let i=0; i<arr.length; i++){
+//     console.log(arr[i]);
+//     result+=arr[i];
+// }
+// console.log(result);
+
+
+// number 6
+
+// let mas = [-1,-3, 5, 7, 1, -9], result=0;
+// for(let i = 0; i<mas.length; i++){
+//     if(mas[i]>0){
+//         result+=mas[i];
+//    }
+// }
+//  console.log(result);
+
+
+// number 7 
+
+// let arr = [1,2,3,4,5,6], result=0;
+// for(let i = 0; i<arr.length; i++){
+//     a = (arr[0]);
+//     b = (arr[1]);
+//     c = (arr[2]);
+//     d = (arr[3]);
+//     g = (arr[4]);
+//     f = (arr[5]);
+//    console.log(a+"-"+b+"-"+c+"-"+d+"-"+g+"-"+f);
+//     }
