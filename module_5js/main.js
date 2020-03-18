790d784e396dcd7cdb19775f7c60cf7be34d2403
Agent222222 <56441631+Agent222222@users.noreply.github.com>
2020-03-18 12:17:01 +0200

    Add files via upload

---
+++ module_5js/main.js
@@ -0,0 +1,76 @@
+// let Arr1 = [1, 5, "4", "hello"],
+//     Arr2 = [true, 2, {}, "a", 222];
+// console.log(Arr1[1]);
+// console.log(Arr2[1]);
+// arr3 = Arr1[1] + Arr2[1];
+// console.log(arr3);
+
+// number 2
+
+// let Arr1 = [1, 5, "4", "hello"];
+// Arr1.push(22);
+// for(let arr of Arr1){
+//      console.log(tepeoff arr);
+// };
+
+// number 3
+
+// let Arr2 = [true, 2, {}, "a", 222];
+// for(let arr of Arr2){
+//     console.log(typeof arr);
+// }
+
+// number 4
+
+// const matrix = [
+//     [10, 11, 12],
+//     [13, 14, 15],
+//     [16, 17, 18]
+// ];
+// for(let i = 0; i<matrix.length; i++){
+//     console.log(matrix[i]);
+//     for(let j = 0; j < matrix[i].length; j++){
+//         console.log(matrix[i][j]);
+//     } 
+// }
+
+// number 5
+
+// const message = 'Welcom to Ukrain!';
+// console.log(message.split(''));
+// console.log(message.indexOf("l"));
+// console.log(message);
+
+// number 6
+
+// let stack = [],
+//     stok = [2, 3, 4],
+//     stk = [5, 6, 7, 8];
+// for(let i = 0; i<stok.length; i++){
+//      stack.unshift(stok[i]);  
+// }
+// for(let j = 0; j<stk.length; j++){
+//     stack.push(stk[j]);
+// }
+// console.log(stack);
+
+// number 7
+
+// let stuck = [4, 3, 2, 5, 6, 7, 8],
+//     lastNumbers = [];
+//     lastNumbers = stuck.splice(stuck.length-3, );
+// console.log(lastNumbers);
+// number 8
+
+// let animals = ["pig", "dog", "cat", "parrot", "chiken"],
+//     pets = [];
+// pets = animals.splice(2, 4);
+// console.log(pets);
+// console.log(animals);
+
+// number 9
+
+// let pets = ["cat", "parrot", "chiken"];
+// const deletedScorces = pets.splice(2, 2);
+// pets.push("shark", "elephant");
+// console.log(pets);
