554468eb13fd6d56a2afd89ecc43c5663822b98b
Agent222222 <56441631+Agent222222@users.noreply.github.com>
2020-04-15 13:02:05 +0300

    Add files via upload

---
+++ module_9js/main.js
@@ -0,0 +1,78 @@
+// number 1
+
+// const sgm = document.querySelector('.sbm');
+// sgm.addEventListener('click', function(event){
+//  event.preventDefault(); 
+//  alert("ти натиснув на кнопку")  
+// });
+
+// number 2
+
+// const toogle = document.querySelector('.toogle');
+// const shavucha = document.querySelector('.shavuha')
+// toogle.addEventListener('click', function(event){
+// if(shavucha.classList.contains("hide")){
+//     shavucha.classList.remove("hide");
+// }else{
+//     shavucha.classList.add("hide");
+// }
+// });
+
+// number 3
+
+// let input = document.querySelector(".phone");
+// input.addEventListener("input", function(){
+//     console.log(input.value);
+// });
+
+// number 4
+
+// input.onblur = function() {
+//       console.log(input.value);
+//   };
+
+// number 5
+
+// window.onload = function() {
+//     alert('Страница загружена');
+//   };
+
+// number 6
+
+// obj = {}; 
+// let input = document.querySelector(".phone"),
+//     inpt = document.querySelector(".name"),
+//     sbm = document.querySelector(".sbm");
+// window.addEventListener("load", function(){
+//     function loadFunc() {
+//         sbm.addEventListener("click", function(){
+//                   obj.name = input.value;
+//                   obj.phone = inpt.value;
+//                   console.log(obj);
+//               }); 
+//     } 
+//     loadFunc();
+// });
+
+
+// number 7
+
+// let img = document.querySelector(".shavuha");
+// img.onmouseover = function(){
+//     alert(document.getElementById('romb').src);
+// };
+
+// number 8
+
+// let input = document.querySelector(".name"),
+//     sbm = document.querySelector(".sbm");
+// function loadFunc() { 
+//     sbm.addEventListener("click", function () {
+//         if(input.value === 'Сергій'){  
+//            alert('Вітаю з перемогою');
+//         }else{
+//           input.value = '';    
+//         }     
+//     });
+// }
+// loadFunc();   
