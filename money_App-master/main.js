4cffaa56454190597088f3fe521e92808fcffd7a
Agent222222 <56441631+Agent222222@users.noreply.github.com>
2020-05-29 16:42:32 +0300

    Add files via upload

--- money_App-master/main.js
+++ money_App-master/main.js
@@ -1,15 +1,9 @@
-let num = document.querySelector(".num"),
-  number = document.querySelector(".number"),
-  fec = document.querySelector(".f-e-c"),
-  botton = document.querySelector(".s2_total-btn");  
-botton.addEventListener("click", function () {
-  sum = document.querySelector(".sum").innerHTML = String(Number(num.value) + Number(number.value) + "uah"
-  );
-});
-let massive = [ 
-    spend = document.createElement("input"),
-    spendsum = document.createElement("input"),
-    ];
+let fec = document.querySelector(".f-e-c"),
+  massive = [
+    (spend = document.createElement("input")),
+    (spendsum = document.createElement("input")),
+  ],
+  totalCalaulation = document.querySelector(".budget_calculation");
 spend.classList.add("enter_costname");
 spendsum.classList.add("enter_costsum");
 fec.append(spend);
@@ -17,94 +11,128 @@ fec.append(spendsum);
 spend.placeholder = "Стаття витрaт";
 spendsum.placeholder = "Сума";
 let plus = document.querySelector(".add-cost"),
-    input = document.getElementById("#number"),
-    total = 0,
-    kd = 0;
-    bring = 0;
-    broung = 0;
-plus.addEventListener("click", function () {
+  input = document.getElementById("#number"),
+  total = 0,
+  j = 1,
+  caught = document.querySelectorAll(".add");
+
+ 
+plus.addEventListener("click", function () { 
+  for(let i = 0; i<1; i++){
+    if(j <= 12){
+      j = j + 1;
 let spend = document.createElement("input"),
     spendsum = document.createElement("input");
-    spend.classList.add("enter_costname");
-    spend.classList.add("added");
-    spendsum.classList.add("added");
-    spendsum.classList.add("enter_costsum");
-    spend.placeholder = "Стаття витрaт";
-    spendsum.placeholder = "Сума";
-    fec.append(spend);
-    fec.append(spendsum);
-
+  spend.classList.add("enter_costname");
+  spend.classList.add("add");
+  spendsum.classList.add("enter_costsum");
+  spendsum.classList.add("add");
+  spend.placeholder = "Стаття витрaт";
+  spendsum.placeholder = "Сума";
+  fec.append(spend);
+  fec.append(spendsum);
+      }else{
+    };
+  };
 });
 let addBotton = document.querySelectorAll(".s2_costs-btn"),
-    form = document.querySelector(".form_enter-costs"),
-    okBotton = document.querySelector(".send-costs"),
-    allSite = document.querySelector(".form"),
-    target; 
+  form = document.querySelector(".form_enter-costs"),
+  okBotton = document.querySelector(".send-costs"),
+  allSite = document.querySelector(".form"),
+  target,
+  broke;
 
+let num = document.querySelector(".num"),
+  number = document.querySelector(".number"),
+  botton = document.querySelector(".s2_total-btn");
+botton.addEventListener("click", function () {  
+ let  sumvitr = document.querySelectorAll(".sp"), 
+      cost = document.querySelector(".cost"),
+      remainder = document.querySelector(".remainder"),
+      vitrats = 0; 
+      cost.innerHTML = "";
+ sumvitr.forEach(function (item, i) {
+    vitrats += Number(item.textContent);
+  });
+  sum = document.querySelector(".sum").innerHTML = String(Number(num.value) + Number(number.value) + "uah");
+  allSite.classList.add("hole");
+  totalCalaulation.style.display = "flex";
+  cost.innerHTML += String(Number(vitrats) + "uah");
+  remainder.innerHTML = String((Number(num.value) + Number(number.value)) - Number(vitrats) + "uah");
+});
 
 addBotton.forEach(function (item) {
   item.addEventListener("click", function () {
-
     allSite.classList.remove("hide");
     form.style.display = "flex";
     allSite.style.alignItems = "center";
     allSite.style.justifyContent = "center";
     target = event.target;
-  })
-});    
+  });
+});
 
 okBotton.addEventListener("click", function () {
-    let broke = document.createElement("span"),
-        brokename = document.createElement("span"),
-        li = document.createElement("li"),
-        img = document.createElement("img"),
-        inputs = document.querySelectorAll(".added");     
-      
-      broke.classList.add("sp");
-      brokename.classList.add("forCloseIcon");
-      li.classList.add("spnd");
-      img.classList.add("delete");
-      li.append(brokename);
-      li.append(broke);
-      li.append(img);
-      li.style.flexDirection = "row";
-      img.src = "./img/close-24px.svg";
-    
-     if (target.classList.contains("main")) {
-        ulon = document.querySelector(".mainCosts-list");
-        ulon.append(li);
-        broke.innerHTML = spendsum.value + " uah";
-        brokename.innerHTML = spend.value;
-        form.classList.add("hide");
-        allSite.classList.add("hide");
-   
+  (spend = document.querySelectorAll(".enter_costname")),
+    (spendsum = document.querySelectorAll(".enter_costsum"));
+  spend.forEach((item, i) => {
+    let brokename = document.createElement("span"),
+      broke = document.createElement("span"),
+      li = document.createElement("li"),
+      valut = (document.createElement("span").innerHTML = "uah "),
+      img = document.createElement("img"),
+      inputs = document.querySelectorAll(".added");
+    broke.classList.add("sp");
+    brokename.classList.add("forCloseIcon");
+    li.classList.add("spnd");
+    img.classList.add("delete");
+    li.append(brokename);
+    li.append(broke);
+    li.append(valut);
+    li.append(img);
+    li.style.flexDirection = "row";
+    img.src = "./img/close-24px.svg";
+    if (isNaN(Number(spendsum[i].value))) {
+      spend[i].value = "";
+      spendsum[i].value = "";
+    } else {
+      if (spendsum[i].value === "") {
+        spend[i].value = "";
+        spendsum[i].value = "";
       } else {
-        ul = document.querySelector(".additCosts-list");
-        ul.append(li);
-        broke.innerHTML = spendsum.value + " uah";
-        brokename.innerHTML = spend.value;
-
+        if (spend[i].value === "") {
+          spend[i].value = "";
+          spendsum[i].value = "";
+        } else {
+          if (target.classList.contains("main")) {
+            ulon = document.querySelector(".mainCosts-list");
+            ulon.append(li);
+            broke.innerHTML = spendsum[i].value;
+            brokename.innerHTML = spend[i].value;
+            form.classList.add("hide");
+            allSite.classList.add("hide");
+          } else {
+            ul = document.querySelector(".additCosts-list");
+            ul.append(li);
+            broke.innerHTML = spendsum[i].value;
+            brokename.innerHTML = spend[i].value;
+          }
+        }
       }
-      form.classList.add("hide");
-      allSite.classList.add("hide");
-      spend.value = "";
-      spendsum.value = "";
-      fec.onclick = function (event) {
-        if (event.target.className != "send-costs") return;
-
-        let pand = event.target.closest(".added");
-        pand.remove();
-      };   
+    }
+    form.classList.add("hide");
+    allSite.classList.add("hide");
+    spend[i].value = "";
+    spendsum[i].value = "";
+    add = document.querySelectorAll(".add");
+    add.forEach(function(item){
+      item.remove();
+    });
+    
   });
 
-allSite.addEventListener("click", function () {
-  // allSite.style.display = 'none';
-  // form.style.display = 'none';
-});
-form.addEventListener("click", function () {
-  form.style.display = "flex";
-});
-
+// caught.classList.add("hide")
+});  
+  
 let deleted = document.querySelector(".delete");
 main = document.querySelector(".mainCosts-list");
 addit = document.querySelector(".additCosts-list");
@@ -120,15 +148,15 @@ addit.onclick = function (event) {
   let pane = event.target.closest(".spnd");
   pane.remove();
 };
-
-//     spendsum.addEventListener('input', updateValue);
-// function updateValue(x) {
-//    for(let i = 0; i<1; i++){
-//   function milliseconds(x) {
-//         if (isNaN(x)) {
-//           spendsum = '';
-//         }
-
-//         console.log(broke);
-//       }
-// }}
+// allSite.addEventListener("click", function () {
+//   allSite.classList.add("hide");
+//   form.style.display = "flex";
+//   console.log(20)
+// });
+form.addEventListener("click", function () {
+  form.style.display = "flex";
+  form.style.width = 1092;
+  form.style.height = 478;
+  allSite.classList.remove("hide");
+  console.log(10)
+});
