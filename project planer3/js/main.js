a680be1c91c603c15e03ebfa7018879af6f2a75b
anigilator479 <56478151+anigilator479@users.noreply.github.com>
2020-06-28 11:26:40 +0300

    Add files via upload

---
+++ project planer3/js/main.js
@@ -0,0 +1,6 @@
+let addcart = document.querySelector(".add"),
+    card = document.querySelector(".card");
+    addcart.addEventListener("click", function () {
+    card.innerHTML += " <div class='card'><div class='mark_btn'><button class='mark'>X</button></div><div class='top'><p class='name'><textarea class='down' placeholder='Name' rows='2' cols='45' name='text'></textarea></p><div class='bot'><div class='description'><textarea class='down' placeholder='Enter note description here' rows='10' cols='45' name='text'></textarea><div class='clock'><input class='clock_input' type='text'></div><button class='clock_btn'></button></div><button class='classification'>Confirm</button><div class='opend_window'><hr class='rope'></hr><p class='Rarity'>Rarity</p><div class='or'><div class='rarity'><div class='Secondly'><input type='radio' id='rarity1' name='rarity' value='1'><label class='label' for='rarity1'>Firstly</label></div><br><div class='Secondly'><input type='radio' id='rarity2' name='rarity' value='2'><label class='label' for='rarity2'>Secondly</label></div></div><br><button class='submit'>Submit</button></div></div></div>";
+    });
+    
