a96bf65c5f5d26e5dfa74ab6f4849d615554a1a8
Agent222222 <56441631+Agent222222@users.noreply.github.com>
2020-06-30 15:48:49 +0300

    Add files via upload

--- project planer3/js/main.js
+++ project planer3/js/main.js
@@ -4,9 +4,21 @@ let addcart = document.querySelector(".add"),
     button = document.querySelector(".add"),
     main = document.querySelector(".main"),
     card = document.querySelector(".card_all");
-    addcart.addEventListener("click", function () {
-    card.insertAdjacentHTML('beforeend', " <div id='card'><div class='mark_btn'><button class='mark'>X</button></div><div class='top<p class='name'><textarea class='down' placeholder='Name' rows='2' cols='45' name='text'></textarea></p><div class='bot'><div class='description'><textarea class='down' placeholder='Enter note description here' rows='10' cols='45' name='text'></textarea><div class='clock'><input class='clock_input' type='text'></div><button class='clock_btn'></button></div><div class='spec'><button class='classification'>Confirm</button><div class='opend_window'><hr class='rope'></hr><p class='Rarity'>Rarity</p><div class='or'><div class='rarity'><div class='Secondly'><input type='radio' id='rarity1' name='rarity' value='1'><label class='label' for='rarity1'>Firstly</label></div><br><div class='Secondly'><input type='radio' id='rarity2' name='rarity'value='2'><label class='label' for='rarity2'>Secondly</label></div></div><br><button class='submit'>Submit</button></div></div></div></div>");
-    });
+addcart.addEventListener("click", function () {
+    card.insertAdjacentHTML('beforeend', " <div id='card'><div class='mark_btn'><button class='mark'>X</button></div><div class='top<p class='name'><textarea class='down' placeholder='Name' rows='2' cols='45' name='text'></textarea></p><div class='bot'><div class='description'><textarea class='down' placeholder='Enter note description here' rows='10' cols='45' name='text'></textarea><div class='clock'><input class='clock_input' type='text'></div><button class='clock_btn'></button></div><div class='spec'><button class='classification'>Confirm</button><div class='opend_window'><hr class='rope'></hr><p class='Rarity'>Rarity</p><div class='or'><div class='rarity'><div class='Firstly'><input class='inputf' type='checkbox' checked id='rarity1' name='rarity' value='1'><label class='label' for='rarity1'>Firstly</label></div><br><div class='Secondly'><input class='inputs' checked type='checkbox' id='rarity2' name='rarity'value='2'><label class='label' for='rarity2'>Secondly</label></div></div><br><button class='submit'>Submit</button></div></div></div></div>");
+let submit = document.querySelector(".submit"),
+    mainCards = document.querySelector(".firstly"),
+    secCards = document.querySelector(".secondly"),
+    inputs = document.querySelectorAll(".input[type=checkbox]");
+submit.addEventListener("click", function(){
+ if(inputs.checked){
+    console.log(10);
+    }
+    else{
+    console.log(20);
+    }
+});
+
 help.addEventListener("click", function(){
     bigtwo.style.display = "flex";
     card.style.display = "none";
@@ -42,4 +54,3 @@ let  number = date.toLocaleString("en-US", options),
              window.setTimeout(arguments.callee, 1000);
          })();
      };
-     
