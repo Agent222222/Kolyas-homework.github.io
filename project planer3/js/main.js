f6f595f56df19fafd29baf715826da938b1b802b
Agent222222 <56441631+Agent222222@users.noreply.github.com>
2020-07-04 21:24:23 +0300

    Add files via upload

--- project planer3/js/main.js
+++ project planer3/js/main.js
@@ -4,7 +4,12 @@ let addcart = document.querySelector(".add"),
     button = document.querySelector(".add"),
     main = document.querySelector(".main"),
     total,   
+    comment = document.querySelector(".comment"),
+    ComentBlock = document.querySelector(".big-three")
     cards = document.querySelector(".card_all");
+comment.addEventListener("click", function(){
+  ComentBlock.style.display = "block";
+});
 addcart.addEventListener("click", function () {
     cards.insertAdjacentHTML('beforeend', " <form class='card'><div class='mark_btn'><button class='mark'>X</button></div><div class='top<p class='name'><textarea class='down' placeholder='Name' rows='2' cols='45' name='text'></textarea></p><div class='bot'><div class='description'><textarea class='down-2' placeholder='Enter note description here' rows='10' cols='45' name='text'></textarea><div class='clock'><input class='clock_input' type='time'><button class='clock_btn'></button></div></div><div class='spec'><button class='classification'>Confirm</button><div class='opend_window'><hr class='rope'></hr><p class='raparity'>Rarity</p><div class='or'><div class='rarity'><div class='Firstly'><input class='inputs' type='radio' id='rarity1' name='rarity' value='1'><label class='label' for='rarity1'>Firstly</label></div><br><div class='Secondly'><input class='inputs' type='radio' id='rarity2' name='rarity'value='2'><label class='label' for='rarity2'>Secondly</label></div></div></div></div></div></form>");
 let  clasific = document.querySelectorAll(".classification"),
@@ -50,39 +55,50 @@ sectionFormRef.forEach(function(item){
  })  
 })
 });
+
+
 mainCards.addEventListener("click", function(){
-let  cardsSec = document.querySelectorAll(".mainCards2"); 
+let cardsMain = document.querySelectorAll(".mainCards1"), 
+    card = document.querySelectorAll(".card"),  
+    cardsSec = document.querySelectorAll(".mainCards2"); 
 cardsSec.forEach(function (item) {
         item.style.display = "none"; 
-  });
-let  cardsMain = document.querySelectorAll(".mainCards1"); 
+  });  
 cardsMain.forEach(function (item) {
         item.style.display = "block"; 
   });
+  ComentBlock.style.display = "none";
 });
 
 
 secCards.addEventListener("click", function(){
-let  cardsMain = document.querySelectorAll(".mainCards1");
+let cardsMain = document.querySelectorAll(".mainCards1"),  
+    cardsSec = document.querySelectorAll(".mainCards2"); 
 cardsMain.forEach(function (item) {
     item.style.display = "none"; 
 });
-let  cardsSec = document.querySelectorAll(".mainCards2"); 
 cardsSec.forEach(function (item) {
         item.style.display = "block"; 
   });
+  ComentBlock.style.display = "none";
 });
+
+
+
 help.addEventListener("click", function(){
     bigtwo.style.display = "flex";
     cards.style.display = "none";
     button.style.display = "none";
+    ComentBlock.style.display = "none";
 });
 main.addEventListener("click", function(){
     bigtwo.style.display = "none";
     cards.style.display = "flex";
     button.style.display = "block";
     button.style.margin = "0px auto";
+    ComentBlock.style.display = "none";
 });
+
 
 const bodyRef = document.getElementsByTagName('body');
 if (bodyRef) {
@@ -97,6 +113,9 @@ function initTheme() {
     document.body.querySelector('.js-switch-input').checked = true;
   };
 };
+
+
+
 let container = document.getElementById("container");
 container.onclick = function(event) {
     if (event.target.className != 'mark') return;
