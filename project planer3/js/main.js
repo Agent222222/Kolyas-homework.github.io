5e6e8bb723fddd0e8b048a710e7e8393b12b108d
Agent222222 <56441631+Agent222222@users.noreply.github.com>
2020-07-04 18:01:22 +0300

    Add files via upload

--- project planer3/js/main.js
+++ project planer3/js/main.js
@@ -3,30 +3,64 @@ let addcart = document.querySelector(".add"),
     bigtwo = document.querySelector(".big-two"),
     button = document.querySelector(".add"),
     main = document.querySelector(".main"),
-    card = document.querySelector(".card_all");
+    total,   
+    cards = document.querySelector(".card_all");
 addcart.addEventListener("click", function () {
-    card.insertAdjacentHTML('beforeend', " <div id='card'><div class='mark_btn'><button class='mark'>X</button></div><div class='top<p class='name'><textarea class='down' placeholder='Name' rows='2' cols='45' name='text'></textarea></p><div class='bot'><div class='description'><textarea class='down' placeholder='Enter note description here' rows='10' cols='45' name='text'></textarea><div class='clock'><input class='clock_input' type='text'></div><button class='clock_btn'></button></div><div class='spec'><button class='classification'>Confirm</button><div class='opend_window'><hr class='rope'></hr><p class='Rarity'>Rarity</p><div class='or'><div class='rarity'><div class='Firstly'><input class='inputf' type='checkbox' checked id='rarity1' name='rarity' value='1'><label class='label' for='rarity1'>Firstly</label></div><br><div class='Secondly'><input class='inputs' checked type='checkbox' id='rarity2' name='rarity'value='2'><label class='label' for='rarity2'>Secondly</label></div></div><br><button class='submit'>Submit</button></div></div></div></div>");
-let submit = document.querySelector(".submit"),
-    mainCards = document.querySelector(".firstly"),
-    secCards = document.querySelector(".secondly"),
-    inputs = document.querySelectorAll(".input[type=checkbox]");
-submit.addEventListener("click", function(){
- if(inputs.checked){
-    console.log(10);
+    cards.insertAdjacentHTML('beforeend', " <form class='card'><div class='mark_btn'><button class='mark'>X</button></div><div class='top<p class='name'><textarea class='down' placeholder='Name' rows='2' cols='45' name='text'></textarea></p><div class='bot'><div class='description'><textarea class='down-2' placeholder='Enter note description here' rows='10' cols='45' name='text'></textarea><div class='clock'><input class='clock_input' type='text'></div><button class='clock_btn'></button></div><div class='spec'><button class='classification'>Confirm</button><div class='opend_window'><hr class='rope'></hr><p class='raparity'>Rarity</p><div class='or'><div class='rarity'><div class='Firstly'><input class='inputs' type='radio' id='rarity1' name='rarity' value='1'><label class='label' for='rarity1'>Firstly</label></div><br><div class='Secondly'><input class='inputs' type='radio' id='rarity2' name='rarity'value='2'><label class='label' for='rarity2'>Secondly</label></div></div></div></div></div></form>");
+let  clasific = document.querySelectorAll(".classification"),
+     opendWind = document.querySelectorAll(".opend_window");
+clasific.forEach(function (item) {
+      item.addEventListener("click", function () {
+        item.nextSibling.style.display = "block";  
+      });
+
+    });
+
+});
+ let  mainCards = document.querySelector(".firstly"),
+    secCards = document.querySelector(".secondly");
+addcart.addEventListener("click", function(){   
+let sectionFormRef = document.querySelectorAll('.rarity');
+// let formRef = document.querySelectorAll('.card');
+sectionFormRef.forEach(function(item){
+    item.addEventListener('input', event => {
+    if (event.target.checked) {
+       event.target.form.classList.add(`mainCards${event.target.value}`);
     }
-    else{
-    console.log(20);
-    }
+ })  })
+     
+
+});
+mainCards.addEventListener("click", function(){
+let  cardsSec = document.querySelectorAll(".mainCards2"); 
+cardsSec.forEach(function (item) {
+        item.style.display = "none"; 
+  });
+let  cardsMain = document.querySelectorAll(".mainCards1"); 
+cardsMain.forEach(function (item) {
+        item.style.display = "block"; 
+  });
 });
 
+
+secCards.addEventListener("click", function(){
+let  cardsMain = document.querySelectorAll(".mainCards1");
+cardsMain.forEach(function (item) {
+    item.style.display = "none"; 
+});
+let  cardsSec = document.querySelectorAll(".mainCards2"); 
+cardsSec.forEach(function (item) {
+        item.style.display = "block"; 
+  });
+});
 help.addEventListener("click", function(){
     bigtwo.style.display = "flex";
-    card.style.display = "none";
+    cards.style.display = "none";
     button.style.display = "none";
 });
 main.addEventListener("click", function(){
     bigtwo.style.display = "none";
-    card.style.display = "flex";
+    cards.style.display = "flex";
     button.style.display = "block";
     button.style.margin = "0px auto";
 });
@@ -34,7 +68,7 @@ main.addEventListener("click", function(){
 let container = document.getElementById("container");
 container.onclick = function(event) {
     if (event.target.className != 'mark') return;
-    let pane = event.target.closest('#card');
+    let pane = event.target.closest('.card');
     pane.remove();
   };
   let date = new Date();
@@ -52,5 +86,5 @@ let  number = date.toLocaleString("en-US", options),
              let time = date.getHours()+':'+date.getMinutes()+':'+date.getSeconds();
              document.querySelector('.clock_input')[0].innerHTML = time;
              window.setTimeout(arguments.callee, 1000);
-         })();
+         });
      };
